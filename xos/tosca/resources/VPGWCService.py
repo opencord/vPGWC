37c5e357e5bb24675924e11f99c56022a5c5eab7
Gabe Black <gabe.black@viavisolutions.com>
2016-11-03 17:41:11 +0000

    Fixed broken python imports
    Change-Id: I114b7a5eef6d623f186e4b00886760a09297c5e0

--- xos/tosca/resources/VPGWCService.py
+++ xos/tosca/resources/VPGWCService.py
@@ -1,13 +1,4 @@
-import os
-import pdb
-import sys
-import tempfile
-sys.path.append("/opt/tosca")
-from translator.toscalib.tosca_template import ToscaTemplate
-import pdb
-
 from services.vpgwc.models import VPGWCService
-
 from service import XOSService
 
 class XOSVPGWCService(XOSService):
